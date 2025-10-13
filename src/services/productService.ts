7be685b180fa0de490b1954ecd62e3ffc79dc666
airKing05 <anil050797@gmail.com>
2025-10-13 07:08:29 +0530

    🔧 chore: update base URL from localhost to local IP

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5001/api/v1"; // change this based on environment
+const API = "http://192.168.43.83:5001/api/v1"  //||  "http://localhost:5001/api/v1"; // change this based on environment
 
 interface dataForComparisonTypes {
   market1Id: string
